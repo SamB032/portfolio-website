9b975721a0f8d28aff458735b9b39f15efdc3c4a
SamB032 <70594311+SamB032@users.noreply.github.com>
2023-04-08 18:12:09 +0100

    Revert "Removed HashBrowser"
    This reverts commit bc49209f7f2c0752270d789243ae1193e3d582d9.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+
+// import { HashRouter as Router} from "react-router-dom";
 
 import './App.css';
 import {useEffect} from 'react';
