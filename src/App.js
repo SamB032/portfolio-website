bc49209f7f2c0752270d789243ae1193e3d582d9
SamB032 <70594311+SamB032@users.noreply.github.com>
2023-04-08 17:58:16 +0100

    Removed HashBrowser

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
-
-// import { HashRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import './App.css';
 import {useEffect} from 'react';
