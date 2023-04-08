c0276adecf5b004ce19f57b905dc619bdd3eec49
SamB032 <70594311+SamB032@users.noreply.github.com>
2023-04-08 18:13:32 +0100

    Completly removed hashbrowser

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
-
-// import { HashRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import './App.css';
 import {useEffect} from 'react';
@@ -11,8 +9,6 @@ import Photography from './pages/Photography';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-
-//! LINK: https://www.youtube.com/watch?v=x7mwVn2z3Sk
 
 function App() {
 
