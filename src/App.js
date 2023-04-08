118bf4ff282182232a811ae18515ff21b4a35c24
SamB032 <70594311+SamB032@users.noreply.github.com>
2023-04-08 17:49:22 +0100

    Added images and changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+
+// import { HashRouter as Router} from "react-router-dom";
+
 import './App.css';
 import {useEffect} from 'react';
 
--- src/helpers/PhotoList.js
+++ src/helpers/PhotoList.js
@@ -4,6 +4,9 @@ import photo3 from "../assets/photo3.jpeg";
 import photo4 from "../assets/photo4.jpeg";
 import photo5 from "../assets/photo5.jpeg";
 import photo6 from "../assets/photo6.jpeg";
+import photo7 from "../assets/photo7.jpeg";
+import photo8 from "../assets/photo8.jpeg";
+import photo9 from "../assets/photo9.jpeg";
 
 export const PhotoList = [
     {image: photo1},
@@ -11,5 +14,8 @@ export const PhotoList = [
     {image: photo3},
     {image: photo4},
     {image: photo5},
-    {image: photo6}
+    {image: photo6},
+    {image: photo7},
+    {image: photo8},
+    {image: photo9}
 ].sort(() => Math.random() - 0.5);
