937eb2d5ca88ff5215fbcdbc87aed4a31f0b7b0c
SamB032 <70594311+SamB032@users.noreply.github.com>
2023-05-24 12:39:30 +0100

    updates website to v2 theme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,46 +5,19 @@ function Home() {
     return (
         <div className="home">
             <div className="about">
-                <h2>Hello, My name is Sam</h2>
-                <div className="prompt">
-                    <p>
+                <div>
+                    <p className="intro">Hello, My name is ...</p>
+                    <p className="name">Sam.</p>
+                    <p className="statement">I'm a Computer Science Undergraduate.</p>
+                </div>
+                
+                <p className="prompt">
                     I am a passionate computer science undergraduate who loves to solve complex problems and create innovative
                     solutions. I have a solid foundation in programming languages like Python, Java, C and JavaScript. 
                     My ultimate goal is to use my technical knowledge and creativity to make a positive impact on the world.
-                    </p>
-                </div>
+                </p>
             </div>
 
-            <div className="skills">
-                <h1>Skills</h1>
-
-                    <ol className="list">
-                        <li className="item">
-                            <h2>Frontend</h2>
-                            <span>
-                                HTML, CSS, React, JavaScript, Qt5, NPM
-                            </span>
-                        </li>
-                    </ol>
-
-                    <ol className="list">
-                        <li className="item">
-                            <h2>Backend</h2>
-                            <span>
-                                SQL, MySQL, SQLite, NodeJS, Java, Python
-                            </span>
-                        </li>
-                    </ol>
-
-                    <ol className="list">
-                        <li className="item">
-                            <h2>Programming Languages</h2>
-                            <span>
-                                Python, Java, C, JavaScript
-                            </span>
-                        </li>
-                    </ol> 
-            </div>
         </div>
     )
 }
