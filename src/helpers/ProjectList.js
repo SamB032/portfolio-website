f16bf7eee53d8c484d64ab5af5bf1b88dedfb815
SamB032 <70594311+SamB032@users.noreply.github.com>
2023-05-24 20:58:16 +0100

    Adds portfolio to projects

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -3,6 +3,7 @@ import proj2 from "../assets/project2.webp";
 import proj3 from "../assets/project3.webp";
 import proj4 from "../assets/project4.webp";
 import proj5 from "../assets/project5.webp";
+import proj6 from "../assets/project6.webp";
 
 export const ProjectList = [
     {
@@ -29,5 +30,10 @@ export const ProjectList = [
         name: "Tetris",
         image: proj5,
         link: "https://github.com/SamB032/pygame-Tetris"
+    },
+    {
+        name: "Portfolio website",
+        image: proj6,
+        link: "https://github.com/SamB032/portfolio-website"
     }
 ];
